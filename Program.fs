7a64763090c37efe108426f476c28de74df72f0f
DriftingHuman091 <104846030+DriftingHuman091@users.noreply.github.com>
2024-10-13 22:35:02 -0400

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿module Main
+open System
+
+[<EntryPoint>]
+let main args =
+    printf "Guess the number between 1 and 100!\n"
+    let number = new Random()
+    let numberChosen = number.Next(1, 100)
+    let rec check_guess() =
+        let mutable guess = 0
+        try
+            guess <- Console.ReadLine() |> int
+        with
+        | :? FormatException -> printf "Not a number\n"; check_guess()
+        match guess with
+        | _ when guess > numberChosen ->
+            printf "Lower! \n" ; check_guess()
+        | _ when guess < numberChosen ->
+            printf "Higher! \n" ; check_guess()
+        | _ when guess = numberChosen ->
+            printf "You win! \n" 
+        | _ -> ()
+
+    check_guess()
+    Console.ReadLine() |> ignore
+    0
